a83d9123a95405d178ad79d0448f404b7105fdd1
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-04-21 12:48:04 -0700

    Implement live new tweet charector counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  const $newTweetText = $('#tweet-text')
+  const $counter = $('.new-tweet .counter')
+
+  $newTweetText.on('input', function() {
+    const count = 140 - $(this).val().length
+    $counter.val(count)
+
+    if (count < 0) {
+      $counter.addClass('red')
+    } else {
+      $counter.removeClass('red')
+    }
+  })
+})
