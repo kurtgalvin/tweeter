9ff86f3a4ec4a8c98579cc6910429e3080bf2bbc
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-04-22 13:09:18 -0700

    Implement a single dynamic tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const createTweetElement = function({user, content}) {
+  return `
+  <article class="tweet">
+    <header>
+      <section>
+        <img src="${user.avatars}" alt="">
+        <span>${user.name}</span>
+      </section>
+      <span class="username">${user.handle}</span>
+    </header>
+
+    <main>
+      <p>${content.text}</p>
+    </main>
+
+    <footer>
+      <time>
+        <span>10</span> days ago
+      </time> 
+      <div class="icons">
+        <span class="material-icons">flag</span>
+        <span class="material-icons">autorenew</span>
+        <span class="material-icons">thumb_up_alt</span>
+      </div>
+    </footer>
+  </article>
+  `
+}
+
+$(document).ready(function() {
+  const $tweet = createTweetElement(tweetData);
+  $('#tweets').append($tweet);
+})
