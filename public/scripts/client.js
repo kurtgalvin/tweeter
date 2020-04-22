45994b550150b5111504aeec8ba377867386aca2
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-04-22 14:09:09 -0700

    Make ajax request for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement = function({user, content}) {
   return `
     <article class="tweet">
@@ -62,18 +37,27 @@ const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  $('#tweets').append(...tweets.map(tweet => {
+  const $tweets = $('#tweets')
+  $tweets.empty()
+  $tweets.append(...tweets.map(tweet => {
     return createTweetElement(tweet)
-  }))
+  }));
+}
+
+const loadTweets = function() {
+  $.get('/tweets/')
+  .then(tweets => {
+    renderTweets(tweets)
+  })
 }
 
 const newTweetSubmit = function(event) {
   event.preventDefault();
-  console.log( $( this ).serialize() );
   $.post({
     url: '/tweets/',
     data: $(this).serialize()
   })
+  .then(_ => loadTweets())
 }
 
 const attachEventHandlers = function() {
@@ -81,6 +65,6 @@ const attachEventHandlers = function() {
 }
 
 $(document).ready(function() {
-  renderTweets(data)
+  loadTweets()
   attachEventHandlers()
 })
