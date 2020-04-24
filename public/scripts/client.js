33d8d1c1f3c68abc2904711bd8637ae0bdafc679
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-04-23 17:26:35 -0700

    Lint

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,26 +6,26 @@
 
 const daysAgoDisplay = function(created_at) {
   // render created_at time in a presentable string
-  console.log(created_at - (1000 * 60 * 60 * 24))
-  const daysAgo = Math.floor((new Date() - created_at) / (1000 * 60 * 60 * 24))
+  console.log(created_at - (1000 * 60 * 60 * 24));
+  const daysAgo = Math.floor((new Date() - created_at) / (1000 * 60 * 60 * 24));
 
   if (daysAgo === 0) {
-    return "Today"
+    return "Today";
   } else if (daysAgo === 1) {
-    return "Yesterday"
+    return "Yesterday";
   } else if (daysAgo <= 10) {
-    return `${daysAgo} days ago`
+    return `${daysAgo} days ago`;
   } else {
-    return "10+ days ago"
+    return "10+ days ago";
   }
 
-}
+};
 
 const escape =  function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
 const createTweetElement = function({user, content, created_at}) {
   return `
@@ -53,79 +53,79 @@ const createTweetElement = function({user, content, created_at}) {
         </div>
       </footer>
     </article>
-  `
-}
+  `;
+};
 
 const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  const $tweets = $('#tweets')
-  $tweets.empty()
+  const $tweets = $('#tweets');
+  $tweets.empty();
   $tweets.append(...tweets.map(tweet => {
-    return createTweetElement(tweet)
+    return createTweetElement(tweet);
   }).reverse());
-}
+};
 
 const loadTweets = function() {
   $.get('/tweets/')
-  .then(tweets => {
-    renderTweets(tweets)
-  })
-}
+    .then(tweets => {
+      renderTweets(tweets);
+    });
+};
 
 const newTweetValidation = function(val) {
   // Resolve promise when tweet value meets requirements
   return new Promise((resolve, reject) => {
     if (!val.trim()) {
-      reject('No Tweet!')
+      reject('No Tweet!');
     } else if (val.length > 140) {
-      reject('Tweet Too Long!')
+      reject('Tweet Too Long!');
     } else {
-      resolve(val)
+      resolve(val);
     }
-  })
-}
+  });
+};
 
 const newTweetSubmit = function(event) {
   // Event handler to render error message or clear and submit tweet
   event.preventDefault();
-  const $this = $(this)
-  const $text = $this.find('textarea')
-  const $warning = $this.find('strong.red')
-  $warning.empty()
+  const $this = $(this);
+  const $text = $this.find('textarea');
+  const $warning = $this.find('strong.red');
+  $warning.empty();
 
   newTweetValidation($text.val())
-  .then(_ => {
-    $.post({
-      url: '/tweets/',
-      data: $this.serialize()
-    }).then(_ => {
-      $text.val('')
-      $('.new-tweet .counter').val(140)
-      loadTweets()
+    .then(_ => {
+      $.post({
+        url: '/tweets/',
+        data: $this.serialize()
+      }).then(_ => {
+        $text.val('');
+        $('.new-tweet .counter').val(140);
+        loadTweets();
+      });
     })
-  })
-  .catch(err => {
-    $warning.append(document.createTextNode(err))
-  })
-}
+    .catch(err => {
+      $warning.append(document.createTextNode(err));
+    });
+};
 
 const toggleDisableNewTweet = function() {
   // Event handler to toggle hiding tweet form
-  $container = $('.container')
-  $container.toggleClass('disable-new-tweet')
+  $container = $('.container');
+  $container.toggleClass('disable-new-tweet');
   if (!$container.hasClass('disable-new-tweet')) {
-    $('#tweet-text').focus()
+    $('#tweet-text').focus();
   }
-}
+};
 
 const attachEventHandlers = function() {
-  $('.new-tweet form').submit(newTweetSubmit)
-  $('#nav-tweet-toggle').click(toggleDisableNewTweet)
-}
+  $('.new-tweet form').submit(newTweetSubmit);
+  $('#nav-tweet-toggle').click(toggleDisableNewTweet);
+};
 
 $(document).ready(function() {
-  loadTweets()
-  attachEventHandlers()
-})
+  loadTweets();
+  attachEventHandlers();
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  const $newTweetText = $('#tweet-text')
-  const $counter = $('.new-tweet .counter')
+  const $newTweetText = $('#tweet-text');
+  const $counter = $('.new-tweet .counter');
 
   $newTweetText.on('input', function() {
-    const count = 140 - $(this).val().length
-    $counter.val(count)
+    const count = 140 - $(this).val().length;
+    $counter.val(count);
 
     if (count < 0) {
-      $counter.addClass('red')
+      $counter.addClass('red');
     } else {
-      $counter.removeClass('red')
+      $counter.removeClass('red');
     }
-  })
-})
+  });
+});
